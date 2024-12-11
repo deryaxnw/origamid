633427262af58538f7b5a8d5d8720b3aa7957643
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-11 14:35:43 -0300

    exercitando

--- funcao/app.js
+++ funcao/app.js
@@ -31,3 +31,66 @@
 // addEventListener('click', () => {
 //     console.log("pd")
 // })
+
+
+// function idade(idade){
+    
+//     if(idade >= 60 || idade >= 27) {
+//         return "velho"
+//     } else if (typeof idade !== "number") {
+//         return false
+//     } else {
+//         return "você é jovem"
+//     }
+// }
+
+// console.log(idade(27))
+
+
+// function paises(paises){
+//     const total = 193;
+
+
+//     return `faltam visitar ${total - paises}`
+
+// }
+
+// console.log(paises(90))
+
+//crie uma função para verficar se o valor e truthy
+
+// function isTruthy(valor) {
+//     return !!valor
+// }
+// console.log(isTruthy(10))
+
+// function quadrado(lado){
+//     return lado + lado
+// }
+
+// console.log(quadrado(4))
+
+// function nome(nome, sobrenome){
+//     return `meu nome é ${nome} ${sobrenome}`
+// }
+
+// console.log(nome("joao", "ryan"))
+
+// function par(num){
+//     modulo = num % 2
+
+//     if(modulo !== 0){
+//         return false
+//     } else {
+//         return "par"
+//     }
+// }
+
+// console.log(par(9))
+
+// function data(data){
+//     return typeof data
+// }
+
+// console.log(data(1))
+
