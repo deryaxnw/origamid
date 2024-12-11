0043c13755391d4de99fef7d5a9f553bdee13b24
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-11 13:09:42 -0300

    cursando origamid pq é legal

---
+++ funcao/app.js
@@ -0,0 +1,18 @@
+// function quadra(){
+//     return 3.14;
+// }
+
+// let num = 5 * quadra();
+
+// console.log(num)
+
+
+// function imc(peso, altura){
+//     let imc = peso / altura * (altura * peso) / altura
+//     return imc
+
+// }
+
+// console.log(imc(20, 300))
+
+
