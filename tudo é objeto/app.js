8a94dc7e43acf2b7d0fd3b1af852634c648fbb7c
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-12 10:18:04 -0300

    methods e propriedades

---
+++ tudo é objeto/app.js
@@ -0,0 +1,15 @@
+const nome = "ryan"
+
+
+console.log(nome.toUpperCase())
+console.log(nome.toLocaleLowerCase())
+console.log(nome.repeat(1000).length)
+
+
+
+// const button = document.querySelector("#b")
+
+// button.addEventListener("click", () => {
+//     console.log("click")
+// })
+
