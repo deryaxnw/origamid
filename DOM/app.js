1c1d8c5d78a69b952dd214b6da92b35f1785a270
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-13 09:54:17 -0300

    dom

---
+++ DOM/app.js
@@ -0,0 +1,14 @@
+const link = window.location.href
+
+console.log(`esse é o link e a rota ${link}`)
+
+const element = document.querySelector(".opa")
+
+function ele(){
+    console.log(`localizei o elemento ${element}`)
+}
+
+addEventListener("click", ele)
+
+
+
