1fe00f54a45c6648c4b3050511cffa64762fcebb
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-15 18:41:27 -0300

    treinando

---
+++ forEach e arrows/app.js
@@ -0,0 +1,30 @@
+
+//retornar todas as images
+
+
+// console.log(img)
+
+
+// const h2g = document.querySelector('.cafe-descricao h2')
+
+// console.log(h2g)
+
+const pp = document.querySelectorAll('p')
+
+// console.log(pp[pp.length - 1])
+
+// let  num = 0
+// const img = document.querySelectorAll("img");
+
+// img.forEach( (item, index, array) => {
+//     console.log(num++)
+    
+// });
+
+// for(num; num <= 10; num++ ){
+//     console.log(num)
+// }
+
+pp.forEach((item, index, array,) => {
+    console.log(item)
+})
