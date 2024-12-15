72573abaf08d2d3c830e1af3e8ff02201ba64f9e
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-15 18:41:39 -0300

    guardar codigo

---
+++ retornando/index.js
@@ -0,0 +1,15 @@
+
+//retornar todas as images
+
+const img = document.querySelectorAll("img");
+
+console.log(img)
+
+
+const h2g = document.querySelector('.cafe-descricao h2')
+
+console.log(h2g)
+
+const pp = document.querySelectorAll('p')
+
+console.log(pp[pp.length - 1])
