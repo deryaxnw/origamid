029acb5025b738a6b4f6b6b649a30191efaebb4b
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-16 15:08:57 -0300

    .

---
+++ distancia/app.js
@@ -0,0 +1,37 @@
+// const img = document.querySelector("img");
+
+// const imgTop = img.offsetTop;
+
+// console.log(imgTop);
+
+// let somar = 0;
+
+// function soma() {
+//   const imgs = document.querySelectorAll("img");
+
+//   imgs.forEach((images) => {
+//     somar = somar + images.offsetWidth;
+
+//   });
+//   console.log(somar)
+// }
+
+// window.onload = () => {
+//     soma()
+// };
+
+// const link = document.querySelectorAll('a')
+
+
+
+// link.forEach((links) => {
+//     const linkWidth = links.offsetWidth;
+//     const linkheight = links.offsetHeight;
+
+//     if(linkWidth >= 48 && linkheight >= 48){
+//         console.log("tem acessibilidade")
+//     } else {
+//         console.log(links, "deve ser ajustado")
+//     }
+// });
+
--- projeto/js/index.js
+++ projeto/js/index.js
@@ -1,1 +1,9 @@
 
+
+const lista = document.querySelector('.cafe-lista')
+const contato = document.querySelector('.contato')
+const title = contato.querySelector('.title')
+
+
+
+lista.appendChild(title)
