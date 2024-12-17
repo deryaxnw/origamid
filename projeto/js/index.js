dc261f3a63553e11145c182093040674786717e6
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-17 08:30:05 -0300

    navegação por tabs

--- projeto/js/index.js
+++ projeto/js/index.js
@@ -1,9 +1,26 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 
-const lista = document.querySelector('.cafe-lista')
-const contato = document.querySelector('.contato')
-const title = contato.querySelector('.title')
+if (tabMenu.length && tabContent.length){
 
+    tabContent[0].classList.add('ativo');
+    
+    function activeTab(index) {
+    
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo')
+        })
+    
+        tabContent[index].classList.add('ativo')
+    }
+    
+    tabMenu.forEach((itemMenu, index) => {
+    
+        itemMenu.addEventListener('click', () => {
+            activeTab(index)
+        })
+    })
+        
+}
 
-
-lista.appendChild(title)
