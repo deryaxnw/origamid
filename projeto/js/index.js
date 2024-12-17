35ea19e3fb7c3434ad1c25e20edf2d0222f3e547
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-17 12:31:39 -0300

    function accordion

--- projeto/js/index.js
+++ projeto/js/index.js
@@ -1,26 +1,49 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTab() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const classActive = 'ativo'
+  //navegação por tab
 
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(classActive);
 
-if (tabMenu.length && tabContent.length){
-
-    tabContent[0].classList.add('ativo');
-    
     function activeTab(index) {
-    
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo')
-        })
-    
-        tabContent[index].classList.add('ativo')
-    }
-    
-    tabMenu.forEach((itemMenu, index) => {
-    
-        itemMenu.addEventListener('click', () => {
-            activeTab(index)
-        })
-    })
-        
-}
+      tabContent.forEach((section) => {
+        section.classList.remove(classActive);
+      });
 
+      tabContent[index].classList.add(classActive);
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  };
+};
+
+initTab();
+
+function acordion() {
+
+  const listaAccordion = document.querySelectorAll(".js-accordion dt");
+  const classActive = 'ativo'
+
+
+  if (listaAccordion.length) {
+    listaAccordion[0].classList.add(classActive);
+    listaAccordion[0].nextElementSibling.classList.add(classActive);
+
+    function activeAccordion() {
+      this.classList.toggle(classActive);
+      this.nextElementSibling.classList.toggle(classActive);
+    }
+
+    listaAccordion.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  };
+};
+
+acordion();
