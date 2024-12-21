ece2f3a796fd2847fca15e32bdd56013a0f73ba9
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-12-21 14:54:22 -0300

    nativos

---
+++ objeto nativo/app.js
@@ -0,0 +1,83 @@
+
+
+//obejtos nativos
+/*
+Object
+String
+Array
+Function
+NUmber
+Math
+*/
+
+
+//obejtos de browser
+
+
+/*  
+    window
+    history
+    Document
+    HTMLCollection
+    NodeList
+*/
+
+
+// const ma = Math.min(1,3,4,5,72,6,32,6); //menor numero
+// const maa = Math.max(1,3,4,5,72,6,32,6);    //maior numero
+
+// // console.log(maa)
+
+// const random = Math.floor(Math.random() * 10)
+
+// console.log(numero)
+
+
+
+// numero aleatorio entre 72 e 40
+
+
+
+// const numero = Math.floor(Math.random() * (72 - 80 + 1)) + 80
+//const numero = Math.floor(Math.random() * (max - min + 1)) + min
+
+
+// console.log(numero)
+
+
+// const mil = Math.floor(Math.random() * (1050 - 2000 + 1)) + 2000
+
+// console.log(mil)
+
+
+// const num = '4, 10, 50, -1'
+// const parse = parseFloat(num)
+// const maior = Math.max(parse)
+
+
+// console.log(maior)
+
+
+// const array = num.split(', ')
+
+// const max = Math.max(...array)
+
+// console.log(max);
+
+// const listaPreco = ['r$ 59,00', 'R$ 100,222', 'R$ 300,99']
+
+// function limparPreco(preco) {
+//     preco = +preco.toUpperCase().replace('R$', '').trim().replace(',' , '.');
+//     preco = +preco.toFixed(1)
+//     return preco
+// }
+
+// let soma = 0
+
+// listaPreco.forEach((preco) => {
+//     soma += limparPreco(preco)
+
+// })
+
+// console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+// limparPreco(listaPreco[1])
