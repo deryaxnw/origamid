8106175bc600d92d87589b493d47f2add1cb4872
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-01-02 09:46:37 -0300

    criando uma todolist

---
+++ todoList/app.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#todo-form')
+const taskTitleInput = document.querySelector('#task-title-input')
+const todoList = document.querySelector('#todo-list')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    
+})
